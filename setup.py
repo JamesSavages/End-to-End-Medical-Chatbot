c10b0730f7d53bfb4676f9fa52b94fdf6db0d6ee
JamesSavage <jamessav2015@outlook.com>
2026-06-07 05:20:45 +1000

    update repo

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = "medical-chatbot",
+    version = "0.1.0",
+    description = "A medical chatbot",
+    author = "James Savage",
+    author_email = "james.savage@outlook.com",
+    packages = find_packages(),
+    install_requires = [ ]
+)
